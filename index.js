58e9789fa65cde80ade69fb46047b9ff07f78725
Junior Vega <guitar.sound12@gmail.com>
2022-06-23 10:58:13 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/* const objeto = [
+    {
+        pregunta: 'holi',
+        id: 1
+    },
+    {
+        pregunta: 'nel hermano',
+        id: 2
+    },
+    {
+        pregunta: 'buberrimo',
+        id: 3
+    }
+]
+
+const texto = document.getElementById('text')
+texto.innerText = objeto[2].pregunta */
